bf390561b7289cfbab48cdf9420645ccdb2cec59
Alexandre Grué <alexandre.grue@hotmail.com>
2022-09-01 10:23:32 +0200

    setup project

---
+++ server/index.ts
@@ -0,0 +1,53 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+            posts: {
+                create: { title: 'Hello World' },
+            },
+            profile: {
+                create: { bio: 'I like turtles' },
+            },
+        },
+    })
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true,
+        },
+    })
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
